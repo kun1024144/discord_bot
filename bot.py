2f54b9535b37c04ed67d61d0556753a9a1cf56ac
kun1024144 <jason621211@gmail.com>
2022-07-25 19:45:13 +0800

    picture

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('settings.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.default()
 
@@ -12,16 +17,27 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1000927978844323891)
+    channel = bot.get_channel(int(jdata['channel']))
     await channel.send(F'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1000927978844323891)
+    channel = bot.get_channel(int(jdata['channel']))
     await channel.send(F'{member} leave!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(F'{round(bot.latency*1000)} (ms)')
 
-bot.run('OTkzOTA4ODM3MzY1MDc2MDA5.GyR1p6.mrdvWNu5mLvBAtvfvCSDk1VM83vI3tcnRikq0o')
+@bot.command()
+async def picture(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file= pic)
+
+@bot.command()
+async def web(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
+bot.run(jdata['TOKEN'])
